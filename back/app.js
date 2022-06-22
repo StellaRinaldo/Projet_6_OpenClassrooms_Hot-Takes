30d7e00aa2da645010a682c82d55e1bc795b3647
Stella Rinaldo <stella.rinaldo95@gmail.com>
2022-06-22 19:08:31 +0200

    Correction de la sécurité avec dotenv

--- back/app.js
+++ back/app.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
+//console.log(process.env);
+
 //Importation de l'application Express
 const express = require('express');
 const path = require('path');//donne accès au chemin de notre systeme de fichier
 
+//REGARDER LE MAIL AVEC LES LIENS POUR LA SECURITE
+/*
+Helmet pour la sécurité : https://www.npmjs.com/package/helmet
+N'oublie pas de rajouter le multer, le middleware authentification dans les requêtes au niveau de la route (précisé également dans le document technique)
+Ensuite tu fais les tests (précisé dans le document sur les étapes du projet) */
+
 //importation de l'application mongoose
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://StellaR:NodokaDB@cluster0.o6rlm.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.o6rlm.mongodb.net/${process.env.DB_NAME}retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
